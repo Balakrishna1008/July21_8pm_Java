2052490fdd8c00b9526558e02b606e6e7d2f23d8
Krish <kbalakrishnan0818@gmail.com>
2025-09-24 13:02:03 +0530

    Updated code

--- eclipse-workspace/Java_July21_Besant/src/com/Filehandling/Methods.java
+++ eclipse-workspace/Java_July21_Besant/src/com/Filehandling/Methods.java
@@ -7,76 +7,77 @@ public class Methods {
 	public static void main(String[] args) {
 		
 		// create new file 
-//		File file = new File("Example2.txt");
+		File file = new File("Example2.txt");
 //		
-//		try {
-//			if(file.createNewFile()) {
-//				System.out.println("File has been created");
-//			}else {
-//				System.out.println("File already exists");
-//			}
-//			
-//		}catch(IOException e){
-//			System.out.println(e);
-//			
-//		}
+		try {
+			if(file.createNewFile()) {
+				System.out.println("File has been created");
+			}else {
+				System.out.println("File already exists");
+			}
+			
+		}catch(IOException e){
+			System.out.println(e);
+			
+		}
+		
 		
 		// write a content in file
 		
-		
-//		 try { 
-//			  FileWriter fw = new FileWriter("Example2.txt");
-//			  fw.write("Hello Everyone"); 
-//			  fw.append("\nThis is Balakrishnan"); // if want to add extra content, use append method
-//			  System.out.println("Content has been written"); 
-//			  fw.close();
-//		  
-//		  }
-//		  catch(IOException e) {
-//			  System.out.println(e); 
-//		  }
+		 try { 
+			  FileWriter fw = new FileWriter("Example2.txt");
+			  fw.write("Hello Everyone"); 
+			  fw.append("\nThis is Balakrishnan"); // if want to add extra content, use append method
+			  System.out.println("Content has been written"); 
+			  fw.close();
+		  
+		  }
+		  catch(IOException e) {
+			  System.out.println(e); 
+		  }
 		 
 		// read a content in a file through character by character
 		
-//		try {
-//			FileReader fr = new FileReader("Example2.txt");
-//			int character;
-//			while((character = fr.read()) != -1) {
-//				System.out.println((char)character);
-//			}
-//			
-//			fr.close();
-//			System.out.println("file read");
-//			
-//		}catch(IOException e) {
-//			System.out.println(e);
-//		}
+		try {
+			FileReader fr = new FileReader("Example2.txt");
+			int character;
+			while((character = fr.read()) != -1) {
+				System.out.println((char)character);
+			}
+			
+			fr.close();
+			System.out.println("file read");
+			
+		}catch(IOException e) {
+			System.out.println(e);
+		}
 //		
 		
 		// read the input using scanner class through word by word
 		
-//		try {
-//			Scanner sc = new Scanner(file);
-//
-//			while(sc.hasNext()) {
-//				
-//				String word = sc.next();
-//				System.out.println(word);
-//			}
-//			
-//			sc.close();
-//			System.out.println("file read");
-//			
-//		}catch(Exception e) {
-//			System.out.println(e);
-//		}
+		try {
+			Scanner sc = new Scanner(file);
+
+			while(sc.hasNext()) {
+				
+				String word = sc.next();
+				System.out.println(word);
+			}
+			
+			sc.close();
+			System.out.println("file read");
+			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
 		
-		//To delete a file 		
-//		if(file.delete()) {
-//			System.out.println("File deleted");
-//		}else {
-//			System.out.println("File not present or already deleted");
-//		}
+		//To delete a file 	
+		
+		if(file.delete()) {
+			System.out.println("File deleted");
+		}else {
+			System.out.println("File not present or already deleted");
+		}
 		
 		
 		// Buffered writer 
@@ -91,13 +92,17 @@ public class Methods {
 			System.out.println(e);
 		}
 		
+		// buffered reader
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("Example1.txt"));
 			String line;
 			while((line = br.readLine()) != null) {
 				System.out.println(line);
 			}
-		}catch(IOException e) {
+			
+			br.close();
+		}
+		catch(IOException e) {
 			System.out.println(e);		}
 	}
 	
